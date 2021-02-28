81e885235f45db923342766b0a7d28433038387a
KhuziamaR <krehman000@citymail.cuny.edu>
2021-02-28 10:54:57 -0500

    Pull To Refresh feature added

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -13,13 +13,17 @@ class HomeTableViewController: UITableViewController {
     var tweetArray = [NSDictionary]()
     var numberOfTweets: Int!
     
+    let myRefreshControl = UIRefreshControl()
+    
     override func viewDidLoad() {
         self.tableView.rowHeight = 120.0
         super.viewDidLoad()
         loadTweets()
+        myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
+        tableView.refreshControl = myRefreshControl
     }
     
-    func loadTweets(){
+    @objc func loadTweets(){
         let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
         let myParams = ["count":10]
         TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success: {
@@ -29,6 +33,7 @@ class HomeTableViewController: UITableViewController {
                 self.tweetArray.append(tweet)
             }
             self.tableView.reloadData()
+            self.myRefreshControl.endRefreshing()
         }, failure: {(Error) in
         print("ERROR - couldnt retrieve data")
     })
